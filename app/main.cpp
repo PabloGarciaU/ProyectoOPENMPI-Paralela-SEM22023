51a970dfa3ea0f6df9e59dd3a3624cdcf284e641
Pablo Garcia Urzua <142696188+PabloGarciaU@users.noreply.github.com>
2023-11-30 02:11:48 -0300

    ordenada estructura
    Acorde a los requerimientos del trabajo, para ejecutar, se debe utilizar el comando: mpirun -hostfile maquinas.txt dist/programa alfa.txt rojo.txt verde.txt azul.txt
    promedio.txt , y por el momento funciona como corresponde, se vee bastante complicado el trabajo pero dare todo mi esfuerzo en tener una nota sobre 50 en caso de que nos vaya mal en el proyecto movil, ahora que tengo tiempo me dedica,e a dar lo mejor de mi para este trabajo, tambien añadi el gitignore pa no subir 100 megas de archivo a cada rato

--- app/main.cpp
+++ app/main.cpp
@@ -1,57 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <mpi.h>
 
-/* cabecera de las llamadas a MPI */
-#include <mpi.h> 
-
-/**
- * Manda un mensaje desde todos los procesos con
- * rango distinto de 0 al proceso 0. El proceso 0
- * los saca por pantalla.
- * Entrada: ninguna
- * Salida: contenido de los mensajes recibidos por 
- * el proceso 0, que los imprime
- */
 int main(int argc, char** argv) {
-    int mi_rango; /* rango del proceso    */
-    int p; /* numero de procesos   */
-    int fuente; /* rango del que envia  */
-    int dest; /* rango del que recibe */
-    int tag = 0; /* etiqueta del mensaje */
-    char mensaje[100]; /* mensaje  */
-    MPI_Status estado; /* devuelve estado al recibir*/
+    int mi_rango, num_procesos;
 
-    /* Comienza las llamadas a MPI */
+    // Inicializa MPI
     MPI_Init(&argc, &argv);
 
-    /* Averiguamos el rango de nuestro proceso */
+    // Obtiene el rango del proceso actual
     MPI_Comm_rank(MPI_COMM_WORLD, &mi_rango);
 
-    /* Averiguamos el número de procesos que estan 
-     * ejecutando nuestro porgrama 
-     */
-    MPI_Comm_size(MPI_COMM_WORLD, &p);
+    // Obtiene el número total de procesos
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procesos);
 
-    if (mi_rango != 0) {
-        /* Crea mensaje */
-        sprintf(mensaje, "Saludos del proceso %d!", mi_rango);
-        dest = 0;
-        /* Usa strlen+1 para que la marca /0 se transmita */
-        MPI_Send(mensaje, strlen(mensaje) + 1, MPI_CHAR,
-                dest, tag, MPI_COMM_WORLD);
-    } else { /* mi_rango == 0 */
-        for (fuente = 1; fuente < p; fuente++) {
-            MPI_Recv(mensaje, 100, MPI_CHAR, fuente,
-                    tag, MPI_COMM_WORLD, &estado);
-            printf("%s\n", mensaje);
-        }
-    }
+    // Código específico de cada proceso
+    printf("Hola desde el proceso %d de %d\n", mi_rango, num_procesos);
 
-    /* Termina con MPI. Recordemos que después de 
-     * esta llamada no podemos llamar a funciones 
-     * MPI, ni siquiera de nuevo a MPI_Init 
-     */
+    // Finaliza MPI
     MPI_Finalize();
+
     return EXIT_SUCCESS;
 }
